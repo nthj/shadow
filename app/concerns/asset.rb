9922874e5c9b9c3a5ce5ff3fafb61403d5514fc0
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-02 16:55:39 -0500

    Fix bucket creation

--- app/concerns/asset.rb
+++ app/concerns/asset.rb
@@ -2,8 +2,10 @@ class Asset < AWS::S3::S3Object
   begin
     set_current_bucket_to ENV['AMAZON_S3_DESTINATION_BUCKET']
     AWS::S3::Bucket.find current_bucket
-  rescue
-    AWS::S3::Bucket.create ENV['AMAZON_S3_DESTINATION_BUCKET'] or raise RuntimeError, "Could not create Asset bucket #{ENV['AMAZON_S3_DESTINATION_BUCKET']}"
+  rescue AWS::S3::NoSuchBucket
+    AWS::S3::Bucket.create ENV['AMAZON_S3_DESTINATION_BUCKET'] or raise RuntimeError, "Could not create Original bucket #{ENV['AMAZON_S3_DESTINATION_BUCKET']}"
+    retry
+  rescue AWS::S3::BucketAlreadyOwnedByYou
     retry
   end
 end
--- app/concerns/original.rb
+++ app/concerns/original.rb
@@ -156,8 +156,10 @@ class Original < AWS::S3::S3Object
   begin
     set_current_bucket_to ENV['AMAZON_S3_SOURCE_BUCKET']
     AWS::S3::Bucket.find current_bucket
-  rescue
+  rescue AWS::S3::NoSuchBucket
     AWS::S3::Bucket.create ENV['AMAZON_S3_SOURCE_BUCKET'] or raise RuntimeError, "Could not create Original bucket #{ENV['AMAZON_S3_SOURCE_BUCKET']}"
+    retry
+  rescue AWS::S3::BucketAlreadyOwnedByYou
     retry
   end
   
