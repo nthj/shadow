cc44e40a3340faebbfea128a773442e2cdf8a8a6
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-02 11:29:59 -0500

    Configure application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,10 @@
 Shadow::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/environment.rb
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = ENV['STYLESHEET_DEVELOPMENT'] || false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
@@ -13,12 +13,14 @@ Shadow::Application.configure do
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
+  config.action_controller.logger          = Logger.new(STDOUT)
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  
+  config.cache_store = :memory_store
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
