81ec23d05ee95b25f72d6b4e1ee05cf18c71eb5a
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-03 17:04:26 -0500

    Setup watermarking

--- app/concerns/original/watermarkable.rb
+++ app/concerns/original/watermarkable.rb
@@ -2,9 +2,13 @@ class Original
   module Watermarkable
     def watermark
       @filters << lambda { |image|
-        # watermark image
+        image.composite!(
+          ::Magick::Image.read(Rails.root.join('config', 'watermark.png')).first, 
+          ::Magick::CenterGravity, 
+          ::Magick::OverCompositeOp
+        )
       }
-      
+
       self
     end
   end
