353c2510a6aa0de769f46c56995a1349979c71be
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-03 17:23:09 -0500

    Change dimensions

--- app/concerns/processors/previewer.rb
+++ app/concerns/processors/previewer.rb
@@ -7,7 +7,7 @@ module Processors
   class Previewer
     class << self
       def perform key
-        Original.find(key).compress(70).resize(250, 250).to(:fit).save(:medium)
+        Original.find(key).compress(70).resize(280, 167).to(:fit).save(:medium)
       end
     end
   end
--- app/concerns/processors/showcaser.rb
+++ app/concerns/processors/showcaser.rb
@@ -8,7 +8,7 @@ module Processors
   class Showcaser
     class << self
       def perform key
-        Original.find(key).compress(100).resize(1200, 1000).to(:fit).watermark.save
+        Original.find(key).compress(100).resize(2500, 1667).to(:fit).watermark.save
       end
     end
   end
