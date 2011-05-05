9c815dbad3958e2b6e8a644b6f317fbc8104b0d0
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-05 18:57:52 -0500

    Queue up to 50 workers at a time

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -6,4 +6,11 @@ Resque::Plugins::HerokuAutoscaler.config do |c|
   c.heroku_pass = ENV['HEROKU_API_KEY'].to_s.split('@').last
   c.heroku_app  = ENV['HEROKU_API_KEY'].to_s.split('@').first
   c.scaling_disabled = Rails.env.development?
+  
+  c.new_worker_count do |pending|
+    return 0 if pending.zero?
+    return 1 if pending < 5
+    return 50 if pending > 50
+    (pending/2).ceil.to_i
+  end
 end
