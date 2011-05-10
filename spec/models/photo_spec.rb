8d7db5c6bfde939b2ce66c5f25e83e99de4bd081
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-10 12:13:21 -0500

    Photo should only carry a save at most once every 3 seconds

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -8,4 +8,10 @@ describe Photo do
     (Photo.new(:etag => 'dc629038ffc674bee6f62eb64ff3a') == o).should be_true
     (Photo.new(:etag => 'non-matching etag') == o).should be_false
   end
+  
+  it "should only carry a save one time in 3 seconds" do
+    Resque.should_receive(:enqueue).once
+    p = Photo.new :etag => 'dc629038ffc674bee6f62eb64ff3a'
+    2.times { p.save }
+  end
 end
