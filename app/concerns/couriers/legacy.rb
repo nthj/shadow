163bb3fb911a2898b28da195146fe8778975cec4
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-10 16:11:55 -0500

    Add Legacy courier

---
+++ app/concerns/couriers/legacy.rb
@@ -0,0 +1,27 @@
+module Couriers
+  class Legacy
+    extend Resque::Plugins::HerokuAutoscaler
+    
+    @queue = 'couriers'
+    
+    class << self
+      attr_accessor :key, :destination
+      
+      def before_perform_log_message id
+        notify "Sending to legacy app", Photo.find(id).key
+      end
+      
+      def execute id
+        Resque.enqueue self, id
+      end
+
+      def perform id
+        Net::HTTP.post_form(
+          URI.parse(destination), 
+          { 'key'       => key, 
+            'filename'  => Photo.find(id).key }
+        )
+      end
+    end
+  end
+end
