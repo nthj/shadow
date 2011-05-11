3de5749631c65be486399a5f85dd8ff830d4f9b0
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-11 15:49:29 -0500

    Only scale every 10 seconds

--- config/initializers/autoscaler.rb
+++ config/initializers/autoscaler.rb
@@ -4,13 +4,17 @@ require 'resque/plugins/resque_heroku_autoscaler'
 module Resque::Plugins::HerokuAutoscaler
   alias set_workers_without_cache set_workers
   def set_workers number_of_workers
-    Rails.cache.write('heroku.worker.count', set_workers_without_cache(number_of_workers), :expires_in => 1.minute)
+    Rails.cache.fetch('heroku.worker.refresh', :expires_in => 10.seconds) do
+      Rails.cache.write('heroku.worker.count', set_workers_without_cache(number_of_workers), :expires_in => 1.minute)
+    end
   end
   
   alias current_workers_without_cache current_workers
   def current_workers
     Rails.cache.fetch('heroku.worker.count', :expires_in => 1.minute) do
-      current_workers_without_cache
+      current_workers_without_cache.tap do |count|
+        Rails.cache.delete 'heroku.worker.refresh' if count.zero?
+      end
     end
   end
 end
