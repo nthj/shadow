841c3af30e350d9c5fc693533b79bcee35e8acf9
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-12 13:29:50 -0500

    Fix is couriable? photo flag

--- app/concerns/plugins/couriable.rb
+++ app/concerns/plugins/couriable.rb
@@ -2,7 +2,6 @@ module Plugins
   module Couriable    
     def self.included base
       base.after_save :carry
-      base.after_save :mark_as_carried
       base.key :carried_at, Time
     end
     
@@ -14,17 +13,13 @@ module Plugins
 
     protected
       def couriable?
-        !self.carried_at? || self.carried_at < 10.seconds.ago
+        !self.updated_at? || self.updated_at < 10.seconds.ago
       end
       
       def couriers
         Dir[Rails.root.join('app', 'concerns', 'couriers', '*.rb')].map do |f| 
           "Couriers::#{File.basename(f, '.rb').classify}".constantize
         end
-      end
-      
-      def mark_as_carried
-        self.carried_at = Time.now
       end
   end
 end
