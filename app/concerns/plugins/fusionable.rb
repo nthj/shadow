034737ad4e07e600118d03eddbb07183ed12ef89
Nathaniel Jones <nathaniel@pointeractive.com>
2011-05-12 19:38:58 -0500

    Change Fusionable dimensions to support portrait orientation

--- app/concerns/plugins/fusionable.rb
+++ app/concerns/plugins/fusionable.rb
@@ -5,9 +5,26 @@ module Plugins
         'geometry'        => point.to_kml,
         'name'            => key,
         'preview'         => Key.new(key).medium,
-        'preview_height'  => dimensions.calculate(Processors::Previewer.width.pixels.wide).height,
-        'preview_width'   => Processors::Previewer.width,
+        'preview_height'  => preview_height,
+        'preview_width'   => preview_width,
         'title'           => title }.filter &:to_s
     end
+    
+    protected
+      def preview_height
+        if orientation.landscape?
+          dimensions.calculate(Processors::Previewer.width.pixels.wide).height
+        else
+          Processors::Previewer.height.pixels.high          
+        end
+      end
+      
+      def preview_width
+        if orientation.landscape?
+          Processors::Previewer.width
+        else
+          dimensions.calculate(Processors::Previewer.height.pixels.high).height
+        end
+      end
   end
 end
